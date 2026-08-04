ad8d983b1aaeec5b3ad0c2c2860f61620e2730e5
mariiamir2018-tech <mariiamir2018@gmail.com>
2026-08-04 19:23:41 +0300

    Добавлен модуль 10.1

---
+++ data/visuals-10-1.js
@@ -0,0 +1,13 @@
+const VISUALS101 = {
+  m101t1: [],
+  m101t2: [],
+  m101t3: [],
+  m101t4: [],
+  m101t5: [],
+  m101t6: [],
+  m101t7: [],
+  m101t8: [],
+  m101t9: []
+};
+
+window.VISUALS101 = VISUALS101;
