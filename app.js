1d2797549346253e5a609e85ec25b540ce8bed00
mariiamir2018-tech <mariiamir2018@gmail.com>
2026-08-05 19:17:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -47,6 +47,25 @@ ${COURSE101.terms.map(x=>`<details><summary>${x[0]}</summary><p>${x[1]}</p></det
 </div>
 
 </section>`;
+</section>`;
+
+const p=$('#summary101');
+
+p.innerHTML+=`
+<section class="final-block">
+<h2>Персоналии ЕГЭ</h2>
+
+<div class="persons-grid">
+${COURSE101.persons.map(x=>`
+<article class="person-ege">
+<h3>${x.name}</h3>
+<p><b>Роль:</b> ${x.role}</p>
+<p class="person-ege-note"><b>Для ЕГЭ:</b> ${x.ege}</p>
+</article>
+`).join('')}
+</div>
+
+</section>`;
 
 }
 
